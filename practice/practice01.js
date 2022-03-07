10dffb9b446d080389f85dd622de0a35276ac110
JP Chyng <43560901+JPChyng@users.noreply.github.com>
2022-03-07 16:06:16 +0800

    Create practice01.js

---
+++ practice/practice01.js
@@ -0,0 +1,10 @@
+function loan(loanData){
+  const captical =loanData.capital,
+        rate = loanData.rate,
+        period = loadData.period;
+  let interest;
+  
+  loadData.interest = captical * rate * period;
+  
+  return loadData;
+ }
