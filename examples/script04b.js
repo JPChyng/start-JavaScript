b5a74706b6af19bba2e67ae26a0ad35ead311521
JP Chyng <43560901+JPChyng@users.noreply.github.com>
2024-02-19 19:32:59 +0800

    Create script04b.js

---
+++ examples/script04b.js
@@ -0,0 +1,17 @@
+/**
+* 
+* @var with for loop
+* 
+*/
+
+console.log('script0a.js');
+
+let	closeUp	= document.querySelector('#closeUp'),
+		pets		= document.querySelector('#pets');
+
+for (let i=0, c=pets.childElementCount; i<c ; i++){
+	pets.children[i].addEventListener("click",function(){
+		closeUp.children[0].textContent	=this.alt;
+		closeUp.children[1].src				=this.src;
+	})
+}
