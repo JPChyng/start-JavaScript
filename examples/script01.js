e63a6328f7b0ac5782442b86a10b0c703c58a540
JP Chyng <43560901+JPChyng@users.noreply.github.com>
2024-02-19 16:47:01 +0800

    script01.js

--- examples/script01.js
+++ examples/script01.js
@@ -1,12 +1,14 @@
-	let	img=document.querySelector('#i1');
+	let img=document.querySelector('#i1');
 console.log(img);
 	img.addEventListener('mouseover',function(){
-		resize(this,2);	});
+		resize(this,2);	
+	});
 	
 	img.addEventListener('mouseout', function(){
-		resize(this,0.5);});
+		resize(this,0.5);
+	});
 		
-	function resize(img,scale){
+	function resize(img, scale){
  		img.width *= scale; 	//寬度伸縮scale倍
   		img.height *= scale; 	//高度伸縮scale倍
 	}
