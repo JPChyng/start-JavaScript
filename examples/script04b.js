a02fd2e67d886ef3378bfab0acbd8d9c5bad6730
JP Chyng <43560901+JPChyng@users.noreply.github.com>
2024-02-19 19:37:06 +0800

    Update script04b.js

--- examples/script04b.js
+++ examples/script04b.js
@@ -1,17 +1,21 @@
 /**
 * 
-* @var with for loop
+* @var case b: with zoomUp function and for loop
 * 
 */
 
-console.log('script0b.js');
+console.log('script04b.js');
 
 let	closeUp	= document.querySelector('#closeUp'),
 		pets		= document.querySelector('#pets');
 
-for (let i=0, c=pets.childElementCount; i<c ; i++){
+function zoomUp(target,source){
+	target.children[0].textContent	=source.alt;
+	target.children[1].src			=source.src;
+}
+
+for	(let i=0, c=pets.childElementCount; i<c ; i++){
 	pets.children[i].addEventListener("click",function(){
-		closeUp.children[0].textContent	=this.alt;
-		closeUp.children[1].src				=this.src;
-	})
+		zoomUp(closeUp,this);
+	});
 }
